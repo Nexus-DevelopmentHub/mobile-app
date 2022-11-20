0a97ec9949973baad37f57b8f15bebb163a8609e
trian damai <triannurizkillah@gmail.com>
2022-11-20 20:28:32 +0700

    refactor: change bg to white splash for testing

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -10,6 +10,9 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      child: Column(),
+    );
   }
 }
