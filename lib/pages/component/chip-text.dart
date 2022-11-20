94b4dc5e3d08f2bcc608e77e6d21b2d09cfc8054
marioprasetyo <kotaksuratprasetyo@gmail.com>
2022-11-20 21:25:33 +0700

    revisi chip text kedua

--- lib/pages/component/chip-text.dart
+++ lib/pages/component/chip-text.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 class Chiptext extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          ChoiceChip(
-            label: Text('Choice 1'),
-            selected: true,
-          ),
-        ],
+    return Center(
+      child: ChoiceChip(
+        label: const Text("popular"),
+        selected: true,
+        selectedColor: Color.fromRGBO(20, 220, 48, 100),
+        onSelected: (newstate){},
       ),
     );
   }
