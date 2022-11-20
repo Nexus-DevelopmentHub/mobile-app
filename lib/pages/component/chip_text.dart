76e1daf4a79c8e3abb2a830e5783794b8e81445f
marioprasetyo <kotaksuratprasetyo@gmail.com>
2022-11-20 21:41:17 +0700

    ganti property

--- lib/pages/component/chip_text.dart
+++ lib/pages/component/chip_text.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 class Chiptext extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ChoiceChip(
+    return Container(
+      child: InputChip(
         label: const Text("popular"),
         selected: true,
-        selectedColor: Color.fromRGBO(20, 220, 48, 100),
         onSelected: (newstate) {},
       ),
     );
