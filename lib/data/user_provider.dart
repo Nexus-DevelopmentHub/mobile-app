176c1507fdb996a96d89ba8dae2e24e979737d7c
trian damai <triannurizkillah@gmail.com>
2022-11-20 15:57:42 +0700

    feat: add firebase

--- lib/data/user_provider.dart
+++ lib/data/user_provider.dart
@@ -2,4 +2,32 @@ import 'package:flutter/foundation.dart';
 
 class UserProvider with ChangeNotifier,DiagnosticableTreeMixin{
 
+  /// TODO :: sign with email and password
+  /// login menggunakan email dan password kemudian mengembalikan callback
+  /// @return {
+  ///   success:boolean,
+  ///   message:string
+  /// }
+  /// **/
+  void signInWithEmailAndPassword(
+      String email,
+      String password,
+      Function(bool success,String message) callback
+  ){
+
+  }
+
+  /// TODO :: sign with google
+  /// login menggunakan akun google
+  /// @return {
+  ///   success:boolean,
+  ///   message:string
+  /// }
+  /// **/
+  void signInWithGoogle(
+      Function(bool success,String message) callback
+      ){
+
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:podcast_app/data/user_provider.dart';
+import 'package:podcast_app/firebase_options.dart';
 import 'package:podcast_app/route/router.dart';
 import 'package:podcast_app/route/routes.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
