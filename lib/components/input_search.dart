d19e0a51cb389916eea2e9ed0b0c6a6ad959d722
masherrs <herrysusanto326@gmail.com>
2022-11-22 17:45:05 +0700

    component input search

--- lib/components/input_search.dart
+++ lib/components/input_search.dart
@@ -6,7 +6,9 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import '../theme/theme.dart';
 
 class InputSearch extends StatefulWidget {
-  const InputSearch({super.key});
+  const InputSearch({super.key, this.onChange});
+
+  final VoidCallback? onChange;
 
   @override
   State<InputSearch> createState() => _InputSearchState();
@@ -19,10 +21,14 @@ class _InputSearchState extends State<InputSearch> {
       TextFormField(
           textInputAction: TextInputAction.search,
           style: Theme.of(context).textTheme.bodyText1,
+          onChanged: (value) {
+            widget.onChange;
+          },
           decoration: InputDecoration(
               hintText: "Find topics, podcaster, etc",
               fillColor: neutral,
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
               focusColor: neutral,
               labelStyle:
                   TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
