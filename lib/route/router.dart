5e0e14298fcc1596c79a3970368bef9a5bac9121
trian damai <triannurizkillah@gmail.com>
2022-11-24 20:10:15 +0700

    feat: add gitignore

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -6,9 +6,9 @@ class RoutePage{
   static Route<dynamic> generateRoute(RouteSettings settings){
     switch(settings.name){
       case Routes.splash:
-        return MaterialPageRoute(builder: (_)=>const SplashScreen());
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
       default:
-        return MaterialPageRoute(builder: (_)=>const SplashScreen());
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
     }
   }
 }
