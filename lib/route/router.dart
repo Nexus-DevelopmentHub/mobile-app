ac31ffdd109c252a040843f743e8dcdf1efd7fe1
trian damai <triannurizkillah@gmail.com>
2022-11-24 21:26:59 +0700

    feat: add page topic
    Refs: #13

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_app/pages/choose_topic.dart';
 import 'package:podcast_app/pages/splash_screen.dart';
 import 'package:podcast_app/route/routes.dart';
 
@@ -7,6 +8,8 @@ class RoutePage{
     switch(settings.name){
       case Routes.splash:
         return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.chooseTopic:
+        return MaterialPageRoute(builder: (_)=>const PageChooseTopic());
       default:
         return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
     }
