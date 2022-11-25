8fb0f51951e0128a36a14d8a25ee2c76883b50f3
marioprasetyo <kotaksuratprasetyo@gmail.com>
2022-11-25 12:36:53 +0700

    card episode

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_app/components/card_episode.dart';
 import 'package:podcast_app/components/input_text.dart';
+import 'package:podcast_app/theme/theme.dart';
 
 class PageSplashScreen extends StatefulWidget {
   const PageSplashScreen({Key? key}) : super(key: key);
@@ -12,14 +14,11 @@ class _PageSplashScreenState extends State<PageSplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: onNeutral,
       body: Column(
         children: [
          //cobain componentnya disini
-         InputText(
-            name: 'Nama',
-            onChange: (value) {},
-            placeholder: 'Masukan Nama Pengguna',
-          ),
+         CardEpisode(name: 'Stories & Cities Jakarta', artist: 'The Fahrul Show', image: 'Mario', time: '1 Hour, 5 min')
         ],
       ),
     );
