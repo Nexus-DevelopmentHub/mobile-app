061809bdaa13968df2dc84af65e5a6ab18668819
trian damai <triannurizkillah@gmail.com>
2022-11-27 16:04:14 +0700

    feat: add route and routes

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -10,6 +10,22 @@ class RoutePage{
         return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
       case Routes.chooseTopic:
         return MaterialPageRoute(builder: (_)=>const PageChooseTopic());
+      case Routes.detailDetailEpisode:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.listEpisode:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.signIn:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.signUp:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.home:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.search:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.detailPodcast:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
+      case Routes.detailPlayer:
+        return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
       default:
         return MaterialPageRoute(builder: (_)=>const PageSplashScreen());
     }
