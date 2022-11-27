a84c62111db7bc0792609c2785e07820f6890b0e
trian damai <triannurizkillah@gmail.com>
2022-11-27 16:19:52 +0700

    feat:  add more pages

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageSplashScreen extends StatefulWidget {
+  const PageSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PageSplashScreen> createState() => _PageSplashScreenState();
+}
+
+class _PageSplashScreenState extends State<PageSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
