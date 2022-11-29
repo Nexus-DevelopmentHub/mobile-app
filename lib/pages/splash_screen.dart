0792e1fbe67e6d2cdeac1b199328dfaaa1ec9a96
trian damai <triannurizkillah@gmail.com>
2022-11-29 22:11:35 +0700

    feat: check user

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_app/data/user_provider.dart';
+import 'package:podcast_app/route/routes.dart';
+import 'package:provider/provider.dart';
 
 class PageSplashScreen extends StatefulWidget {
   const PageSplashScreen({Key? key}) : super(key: key);
@@ -8,6 +11,20 @@ class PageSplashScreen extends StatefulWidget {
 }
 
 class _PageSplashScreenState extends State<PageSplashScreen> {
+  @override
+  void initState() {
+    /**
+     * when user already logged in redirect to home and otherwise redirect to sign in
+     * */ 
+    context.read<UserProvider>().checkIsLoggedIn().then((isLoggedIn) => {
+      if(isLoggedIn){
+        Navigator.of(context).pushNamed(Routes.home)
+      }else{
+        Navigator.of(context).pushNamed(Routes.signIn)
+      }
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
