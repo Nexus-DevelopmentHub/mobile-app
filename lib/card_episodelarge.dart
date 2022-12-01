bdff2f756e4152ac7f40805c2a107b2b9166c833
Fahrulgans <fahruliswandika@gmail.com>
2022-12-01 23:14:50 +0700

    list_episode

---
+++ lib/card_episodelarge.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:podcast_app/theme/theme.dart';
+
+class CardEpisodeLarge extends StatefulWidget {
+  const CardEpisodeLarge(
+      {super.key,
+      required this.name,
+      required this.artist,
+      required this.image,
+      required this.time});
+
+  final String name;
+  final String artist;
+  final String image;
+  final String time;
+
+  @override
+  State<CardEpisodeLarge> createState() => _CardEpisodeLargeState();
+}
+
+class _CardEpisodeLargeState extends State<CardEpisodeLarge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 156,
+      height: 220,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 156,
+            height: 156,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image(
+                  image: NetworkImage(widget.image),
+                )),
+          ),
+          SizedBox(height: 4),
+          Container(
+            child: (Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.name,
+                  style: TextStyle(
+                    color: neutral,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  widget.artist,
+                  style: TextStyle(
+                    color: subtitle,
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  widget.time,
+                  style: TextStyle(
+                    color: subtitle,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/list_episode.dart
+++ lib/pages/list_episode.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:podcast_app/components/card_episode.dart';
+import '../components/input_search.dart';
+import '../theme/theme.dart';
 
 class PageListEpisode extends StatefulWidget {
   const PageListEpisode({Key? key}) : super(key: key);
@@ -10,6 +13,114 @@ class PageListEpisode extends StatefulWidget {
 class _PageListEpisodeState extends State<PageListEpisode> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: onNeutral,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverPadding(
+            padding: EdgeInsets.only(
+              left: 4,
+              right: 8,
+            ),
+            sliver: SliverAppBar(
+              leading: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                    height: 40,
+                    width: 40,
+                    child: IconButton(
+                        onPressed: () {}, icon: Icon(Icons.arrow_back)),
+                  )
+                ],
+              ),
+              title: InputSearch(
+                  name: '',
+                  onChange: ((p0) {}),
+                  placeholder: 'Find topics, podcaster, etc'),
+              toolbarHeight: 72,
+              forceElevated: innerBoxIsScrolled,
+              backgroundColor: onNeutral,
+              pinned: true,
+            ),
+          ),
+        ],
+        body:GridView.count(
+  primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 10,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  children: <Widget>[
+    Container(
+     height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CardEpisode(
+                                totalIndex: 2,
+                                index: index,
+                                name: 'Stories & Cities Jakarta',
+                                artist: 'The Fahrul Show',
+                                image:
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXVBhpoyFQDdjlBcLiiu7i-TU0hlM5iBVdQ&usqp=CAU',
+                                time: '1 Hour, 5 Min');
+                          }),
+    ),
+    Container(
+height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CardEpisode(
+                                totalIndex: 2,
+                                index: index,
+                                name: 'Stories & Cities Jakarta',
+                                artist: 'The Fahrul Show',
+                                image:
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXVBhpoyFQDdjlBcLiiu7i-TU0hlM5iBVdQ&usqp=CAU',
+                                time: '1 Hour, 5 Min');
+                          }),
+    ),
+    Container(
+height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CardEpisode(
+                                totalIndex: 2,
+                                index: index,
+                                name: 'Stories & Cities Jakarta',
+                                artist: 'The Fahrul Show',
+                                image:
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXVBhpoyFQDdjlBcLiiu7i-TU0hlM5iBVdQ&usqp=CAU',
+                                time: '1 Hour, 5 Min');
+                          }),
+    ),
+    Container(
+height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CardEpisode(
+                                totalIndex: 2,
+                                index: index,
+                                name: 'Stories & Cities Jakarta',
+                                artist: 'The Fahrul Show',
+                                image:
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXVBhpoyFQDdjlBcLiiu7i-TU0hlM5iBVdQ&usqp=CAU',
+                                time: '1 Hour, 5 Min');
+                          }),
+
+    ),
+  ],
+)
+          ),
+    );
   }
 }
