840a20c6a02de5149dc8c0a7e56f78df53eb2f9f
trian damai <triannurizkillah@gmail.com>
2022-12-03 00:09:32 +0700

    feat: finishing model

--- lib/models/episode_model.dart
+++ lib/models/episode_model.dart
@@ -1,3 +1,47 @@
-class EpisodeModel{
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class EpisodeModel{
+  String? title="";
+  String? description="";
+  String? thumbnail="";
+  String? createdBy="";
+  int? durationInSeconds=0;
+  String? podcastId="";
+
+  EpisodeModel({
+    this.title,
+    this.description,
+    this.thumbnail,
+    this.createdBy,
+    this.durationInSeconds,
+    this.podcastId,
+  });
+
+
+  factory EpisodeModel.fromFirestore(
+      DocumentSnapshot<Map<String,dynamic>> snapshot,
+      SnapshotOptions? options
+      ){
+    final data = snapshot.data();
+    return EpisodeModel(
+        title: data?['title'],
+        description: data?['description'],
+        thumbnail: data?['thumbnail'],
+        createdBy: data?['createdBy'],
+        durationInSeconds: data?['durationInSeconds'],
+        podcastId: data?['podcastId']
+    );
+
+  }
+
+  Map<String,Object?> toFirestore(){
+    return {
+      if(title != null) "title":title,
+      if(description != null) "description":description,
+      if(thumbnail != null) "thumbnail":thumbnail,
+      if(createdBy != null) "createdBy":createdBy,
+      if(durationInSeconds != null) "durationInSeconds":durationInSeconds,
+      if(podcastId != null) "podcastId":podcastId
+    };
+  }
 }
--- lib/models/podcast_model.dart
+++ lib/models/podcast_model.dart
@@ -1,1 +1,46 @@
-class PodcastModel{}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PodcastModel {
+  String? title = "";
+  String? description = "";
+  String? thumbnail = "";
+  int? likes = 0;
+  int? createdAt = 0;
+  String? createdBy = "";
+  String? id = "";
+
+  PodcastModel(
+      {this.title,
+      this.description,
+      this.thumbnail,
+      this.likes,
+      this.createdAt,
+      this.createdBy,
+      this.id});
+
+  factory PodcastModel.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data();
+    return PodcastModel(
+        title: data?['title'],
+        description: data?['description'],
+        thumbnail: data?['thumbnail'],
+        likes: data?['likes'],
+        createdBy: data?['createdBy'],
+        createdAt: data?['createdAt'],
+        id: data?['id']);
+  }
+
+  Map<String, Object?> toFirestore() {
+    return {
+      if (title != null) "title": title,
+      if (description != null) "description": description,
+      if (thumbnail != null) "thumbnail": thumbnail,
+      if (likes != null) "likes": likes,
+      if (createdBy != null) "createdBy": createdBy,
+      if (createdAt != null) "createdAt": createdAt,
+      if (id != null) "id": id
+    };
+  }
+}
