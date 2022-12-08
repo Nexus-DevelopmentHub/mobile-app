8f1ccf79a886fb488a3900534d907350974090f3
marioprasetyo <kotaksuratprasetyo@gmail.com>
2022-12-08 11:21:35 +0700

    page player

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:podcast_app/components/slider_player.dart';
 import 'package:podcast_app/pages/header_player.dart';
 import 'package:podcast_app/theme/theme.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
 class PagePlayer extends StatefulWidget {
-  const PagePlayer({super.key});
+  const PagePlayer({
+    super.key,
+  });
 
   @override
   State<PagePlayer> createState() => _PagePlayerState();
@@ -14,39 +18,85 @@ class PagePlayer extends StatefulWidget {
 class _PagePlayerState extends State<PagePlayer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: onNeutral,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
-        backgroundColor: onNeutral,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.more_vert_rounded),
-            onPressed: () {},
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 8,
           ),
-        ],
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 8,
-            ),
-            HeaderPlayer(
+          Container(
+            margin: EdgeInsets.only(left: 24, right: 24),
+            child: HeaderPlayer(
               createdBy: 'The Fahrul Show',
               title: 'Stories & Cities Jakarta Episode 2',
               thumbnail: 'thumbnail',
             ),
-            const SizedBox(
-              height: 24,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 24, right: 24),
+            child: SliderPlayer(),
+          ),
+          const SizedBox(
+            height: 68,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 24, right: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 40,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 32,
+                        width: 32,
+                        child: Image(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                            'lib/icons/conect.png',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Connect Device',
+                        style: TextStyle(
+                          color: primary,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.keyboard_arrow_down_rounded,
+                          color: neutral,
+                          size: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.airplay_rounded,
+                    color: neutral,
+                    size: 24,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
